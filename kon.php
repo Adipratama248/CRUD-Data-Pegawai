952b0115f1df68c1edbff60fc45bd1bfce0a5ea9
Adi.pratama <156062888+Adipratama248@users.noreply.github.com>
2024-09-22 15:25:51 +0700

    Delete kon.php

--- kon.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn=mysqli_connect('localhost','root','','kantor');
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-?>
